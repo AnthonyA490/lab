7d50534f8946eb03db15ae57227ed8cd4f1189f7
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-10-11 10:38:25 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,13 +13,13 @@ function _7 () {
     pins.digitalWritePin(DigitalPin.P15, 1)
 }
 function _2 () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 }
 function _6 () {
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -50,6 +50,7 @@ input.onButtonPressed(Button.A, function () {
     _8()
     basic.pause(1000)
     _9()
+    basic.pause(1000)
 })
 function _9 () {
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -61,10 +62,10 @@ function _9 () {
     pins.digitalWritePin(DigitalPin.P15, 0)
 }
 function _5 () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P12, 1)
+    pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
@@ -97,12 +98,12 @@ function _0 () {
     pins.digitalWritePin(DigitalPin.P15, 1)
 }
 function _4 () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P12, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
 }
 function _1 () {
